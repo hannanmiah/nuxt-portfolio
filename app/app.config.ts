5ac9826472428ad7669392932ea47aca3b04c483
Hannan Miah <hannanhridoy@gmail.com>
2025-11-23 19:57:41 +0600

    chore: refactor image and title

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
   global: {
     picture: {
-      dark: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'My profile picture'
+      dark: '/photo.jpg',
+      light: '/photo.jpg',
+      alt: 'Hannan Miah'
     },
-    meetingLink: 'https://cal.com/',
-    email: 'ui-pro@nuxt.com',
+    meetingLink: 'tel:+8801787378887',
+    email: 'hannanhridoy@gmail.com',
     available: true
   },
   ui: {
@@ -23,23 +23,23 @@ export default defineAppConfig({
     }
   },
   footer: {
-    credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
+    credits: `Hannan Miah • © ${new Date().getFullYear()}`,
     colorMode: false,
     links: [{
-      'icon': 'i-simple-icons-discord',
-      'to': 'https://go.nuxt.com/discord',
+      'icon': 'i-simple-icons-facebook',
+      'to': 'https://fb.me/hannanmiah29',
       'target': '_blank',
-      'aria-label': 'Nuxt on Discord'
+      'aria-label': 'Connect on Facebook'
     }, {
-      'icon': 'i-simple-icons-x',
-      'to': 'https://go.nuxt.com/x',
+      'icon': 'i-simple-icons-linkedin',
+      'to': 'https://linkedin.com/in/hannanmiah',
       'target': '_blank',
-      'aria-label': 'Nuxt on X'
+      'aria-label': 'Connect on LinkedIn'
     }, {
       'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt/ui',
+      'to': 'https://github.com/hannanmiah',
       'target': '_blank',
-      'aria-label': 'Nuxt UI on GitHub'
+      'aria-label': 'Connect on GitHub'
     }]
   }
 })
